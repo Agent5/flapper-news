d10ecb82442be6f78d2f5747571925df9f47684b
Eddie Naff <eddienaff@gmail.com>
2015-03-20 16:25:28 -0700

    persistent comments to db with upvoting

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,96 +1,106 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-router.get('/posts', function(req, res, next) {
-    Post.find(function(err, posts) {
-        if(err){ return next(err); }
+// Preload post objects on routes with ':post'
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
 
-        res.json(posts);
-    });
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+// Preload comment objects on routes with ':comment'
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
 });
 
 router.post('/posts', function(req, res, next) {
-    var post = new Post(req.body);
+  var post = new Post(req.body);
 
-    post.save(function(err, post) {
-        if(err){ return next(err); }
+  post.save(function(err, post){
+    if(err){ return next(err); }
 
-        res.json(post);
-    });
+    res.json(post);
+  });
 });
 
+
+// return a post
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    return res.json(post);
+  });
+});
+
+
+// upvote a post
 router.put('/posts/:post/upvote', function(req, res, next) {
-    req.post.upvote(function(err, post) {
-        if (err) { return next(err); }
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
 
-        res.json(post);
-    });
+    res.json(post);
+  });
 });
 
-router.get('/posts/:post', function(req, res) {
-    req.post.populate('comments', function(err, post) {
-        if (err) { return next(err); }
-    });
-    res.json(req.post);
-});
 
-router.param('post', function(req, res, next, id) {
-    var query = Post.findById(id);
-
-    query.exec(function (err, post) {
-        if (err) { return next(err); }
-        if (!post) { return next(new Error('can\'t find post')); }
-
-        req.post = post;
-        return next();
-    });
-
-});
-
+// create a new comment
 router.post('/posts/:post/comments', function(req, res, next) {
-    var comment = new Comment(req.body);
-    comment.post = req.post;
+  var comment = new Comment(req.body);
+  comment.post = req.post;
 
-    comment.save(function(err, comment) {
-        if(err){ return next(err); }
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
 
-        req.post.comments.push(comments);
-        req.post.save(function(err, post) {
-            if(err) { return next(err); }
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
 
-            res.json(comment);
-        });
+      res.json(comment);
     });
+  });
 });
 
+
+// upvote a comment
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
-    req.comment.upvote(function(err, post) {
-        if (err) { return next(err); }
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
 
-        res.json(comment);
-    });
-});
-
-router.param('comment', function(req, res, next, id) {
-    var query = Comment.findById(id);
-
-    query.exec(function (err, comment) {
-        if (err) { return next(err); }
-        if (!post) { return next(new Error('can\'t find comment')); }
-
-        req.comment = comment;
-        return next();
-    });
-
-});
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.json(comment);
+  });
 });
 
 module.exports = router;
