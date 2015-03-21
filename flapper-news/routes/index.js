f476d3155ceb5c14f303ac39bbc5c5074b5e7094
Eddie Naff <eddienaff@gmail.com>
2015-03-21 10:09:35 -0700

    login and authentication with passport.js added

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,15 +1,19 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-
-
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var Comment = mongoose.model('Comment');
 
 // Preload post objects on routes with ':post'
 router.param('post', function(req, res, next, id) {
@@ -45,8 +49,9 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post){
     if(err){ return next(err); }
@@ -67,7 +72,7 @@ router.get('/posts/:post', function(req, res, next) {
 
 
 // upvote a post
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -77,9 +82,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 
 
 // create a new comment
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
@@ -95,12 +101,47 @@ router.post('/posts/:post/comments', function(req, res, next) {
 
 
 // upvote a comment
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
     res.json(comment);
   });
+});
+
+// register for a user account
+router.post('/register', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function (err) {
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()});
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if(err) { return next(err); }
+
+    if(user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
